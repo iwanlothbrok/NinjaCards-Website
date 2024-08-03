a84fb2d3fcc3c3fc507a7c206ae0302e274db6bf
Iwan <iwaniwanow0345@gmail.com>
2024-08-03 23:36:00 +0300

    initial

---
+++ ninja-cards/app/components/ProductCard.tsx
@@ -0,0 +1,10 @@
+'use client'; // you are making it client component - so you can use useeffect, usefunc and etc 
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>ProductCard</div>
+  )
+}
+
+export default ProductCard
