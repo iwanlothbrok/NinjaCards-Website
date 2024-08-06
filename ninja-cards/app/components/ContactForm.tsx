8c305020c590ad73a7ed3ba49d6bc41eba41ae49
Iwan <iwaniwanow0345@gmail.com>
2024-08-06 14:39:20 +0300

    next-auth used for JWT

--- ninja-cards/app/components/ContactForm.tsx
+++ ninja-cards/app/components/ContactForm.tsx
@@ -6,33 +6,33 @@ const ContactForm: React.FC = () => {
     return (
         <form className="max-w-lg mx-auto">
             <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                <label className="block text-orange text-sm font-bold mb-2" htmlFor="name">
                     Name
                 </label>
                 <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-orange leading-tight focus:outline-none focus:shadow-outline"
                     id="name"
                     type="text"
                     placeholder="Your name"
                 />
             </div>
             <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                <label className="block text-orange text-sm font-bold mb-2" htmlFor="email">
                     Email
                 </label>
                 <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-orange leading-tight focus:outline-none focus:shadow-outline"
                     id="email"
                     type="email"
                     placeholder="Your email"
                 />
             </div>
             <div className="mb-6">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
+                <label className="block text-orange text-sm font-bold mb-2" htmlFor="message">
                     Message
                 </label>
                 <textarea
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-orange leading-tight focus:outline-none focus:shadow-outline"
                     id="message"
                     placeholder="Your message"
                     rows={4}
