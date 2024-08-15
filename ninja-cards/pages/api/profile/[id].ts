31dc5fa6234ce95f902e72de17d20836df31cc69
Iwan <iwaniwanow0345@gmail.com>
2024-08-15 13:49:51 +0300

    get user by id api was added

---
+++ ninja-cards/pages/api/profile/[id].ts
@@ -0,0 +1,33 @@
+// File: pages/api/users/[id].ts
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { id } = req.query;
+
+    console.log('id in api is ' + id);
+
+    if (req.method !== 'GET') {
+        return res.status(405).json({ message: 'Method not allowed' });
+    }
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: Number(id), // Convert the id to a number if it's stored as an integer
+            },
+        });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        return res.status(200).json(user);
+    } catch (error) {
+        console.error('Error fetching user:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
