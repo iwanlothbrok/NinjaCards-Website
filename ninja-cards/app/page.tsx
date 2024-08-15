0bb654acc6a4368965bcec79a4a7308f25dd7e7e
al-badarin <jamalalbadarin@gmail.com>
2024-08-15 14:31:59 +0300

    remove unnecessary imports

--- ninja-cards/app/page.tsx
+++ ninja-cards/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-import Link from 'next/link'
 import { HomePage } from './components/Home'
 import Features from './components/Features'
 import About from './components/About'
