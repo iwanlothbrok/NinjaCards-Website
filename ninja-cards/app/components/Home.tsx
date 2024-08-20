c4423f5fbfc4dc9c9bc155fa4143a68cd558f4a3
Iwan <iwaniwanow0345@gmail.com>
2024-08-20 11:49:54 +0300

    frame package update

--- ninja-cards/app/components/Home.tsx
+++ ninja-cards/app/components/Home.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export const HomePage: React.FC = () => {
     return (
-        <section className="w-full h-screen bg-cover bg-center relative" style={{ backgroundImage: 'url(/nfc-card.webp  )' }}>
+        <section className="w-full h-screen bg-cover bg-center relative" style={{ backgroundImage: 'url(/nfc-card.webp)' }}>
             <div className="bg-black bg-opacity-75 w-full h-full flex flex-col items-center justify-center text-center text-white">
                 <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to Smart NFC Cards</h1>
                 <p className="text-lg md:text-2xl mb-8">Innovative solutions for modern needs</p>
