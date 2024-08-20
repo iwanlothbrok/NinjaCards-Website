af0a6fc8a11e84a5eefa85f8b6a902a5d2075f76
Iwan <iwaniwanow0345@gmail.com>
2024-08-20 11:54:35 +0300

    new things used

--- ninja-cards/app/page.tsx
+++ ninja-cards/app/page.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import { HomePage } from './components/Home'
 import Features from './components/Features'
 import About from './components/About'
 import Roadmap from './components/Roadmap'
 import VideoTextSection from './components/VideoTextSection'
 import FeaturesSection from './components/FeaturesSection'
+import Testimonies from './components/Testimonies'
+import { motion } from 'framer-motion';
+import ProductGallery from './components/ProductCard';
+import Product from './components/Product';
 
 export default function Home() {
   return (
@@ -13,6 +19,8 @@ export default function Home() {
       <About />
       <VideoTextSection />
       <Roadmap />
+      <Testimonies />
+      <ProductGallery />
       <FeaturesSection />
     </main>
   )
