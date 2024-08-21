0edcc3c0d8f25216ead4f1ac78ae697960abc634
al-badarin <jamalalbadarin@gmail.com>
2024-08-22 01:10:42 +0300

    create FAQ page.tsx

---
+++ ninja-cards/app/askedQuestions/page.tsx
@@ -0,0 +1,11 @@
+import FrequentlyAskedQuestions from '../components/FrequentlyAskedQuestions';
+
+const FAQPage = () => {
+  return (
+    <div className='mt-24'>
+      <FrequentlyAskedQuestions />;
+    </div>
+  );
+};
+
+export default FAQPage;
