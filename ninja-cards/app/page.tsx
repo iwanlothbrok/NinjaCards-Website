29a924110d69fa2b23fd78120cbd19bc98af9e3d
al-badarin <jamalalbadarin@gmail.com>
2024-08-23 22:33:30 +0300

    Update page.tsx

--- ninja-cards/app/page.tsx
+++ ninja-cards/app/page.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import { HomePage } from './components/Home'
-import Features from './components/Features'
-import About from './components/About'
-import Roadmap from './components/Roadmap'
-import Roadmap2 from './components/Roadmap2'
-import Roadmap3 from './components/Roadmap3'
-import VideoTextSection from './components/VideoTextSection'
-import FeaturesSection from './components/FeaturesSection'
-import Testimonies from './components/Testimonies'
+import { HomePage } from './components/Home';
+import Features from './components/Features';
+import About from './components/About';
+import Roadmap from './components/Roadmap';
+import Roadmap2 from './components/Roadmap2';
+import Roadmap3 from './components/Roadmap3';
+import VideoTextSection from './components/VideoTextSection';
+import FeaturesSection from './components/FeaturesSection';
+import Testimonies from './components/Testimonies';
 import { motion } from 'framer-motion';
 import ProductGallery from './components/ProductCard';
 import FrequentlyAskedQuestions from './components/FrequentlyAskedQuestions';
@@ -26,7 +26,7 @@ export default function Home() {
       <Testimonies />
       <ProductGallery />
       <FeaturesSection />
-      <FrequentlyAskedQuestions/>
+      <FrequentlyAskedQuestions searchTerm={''} />
     </main>
-  )
+  );
 }
