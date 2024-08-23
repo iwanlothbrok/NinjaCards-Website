cbdb900d3c8ed849106ca0daa5903192405243f8
al-badarin <jamalalbadarin@gmail.com>
2024-08-23 20:13:33 +0300

    refactor iphone mochup with tailwind css

--- ninja-cards/app/components/About.tsx
+++ ninja-cards/app/components/About.tsx
@@ -30,10 +30,13 @@ const About: React.FC = () => {
   return (
     <div className="bg-black text-white flex flex-col md:flex-row items-center justify-between p-8 md:p-16">
       <div className="max-w-md">
-        <h2 className="text-3xl md:text-4xl font-bold mb-4">Why is it so great?</h2>
+        <h2 className="text-3xl md:text-4xl font-bold mb-4">
+          Why is it so great?
+        </h2>
         <p className="text-gray-400 mb-8">
-          Nunc tincidunt vulputate elit. Mauris varius purus malesuada neque iaculis malesuada.
-          Aenean gravida magna orci, non efficitur est porta id. Donec magna diam.
+          Nunc tincidunt vulputate elit. Mauris varius purus malesuada neque
+          iaculis malesuada. Aenean gravida magna orci, non efficitur est porta
+          id. Donec magna diam.
         </p>
         <div className="grid grid-cols-2 gap-4 text-sm">
           <div>
@@ -72,45 +75,66 @@ const About: React.FC = () => {
         </div>
       </div>
 
-      <div className="mt-8 md:mt-0">
-        {/* iPhone mockup */}
-        <div className="relative flex justify-center items-center p-4 bg-black rounded-3xl shadow-lg" style={{ width: '300px', height: '600px' }}>
-          <div className="absolute inset-0 bg-black rounded-2xl" style={{ padding: '20px', border: '10px solid #333', borderRadius: '30px' }}>
-            <div className="bg-black w-full h-full rounded-lg overflow-hidden shadow-inner">
-              <div className="bg-white text-black text-center p-6">
-                <p className="text-lg">Good evening,</p>
-                <p className="text-2xl font-bold">Friday, October 2</p>
-                <p className="text-sm text-gray-500">Mahatma Gandhi Jayanti</p>
-              </div>
-              <div className="flex justify-around mt-8">
-                <button className="w-10 h-10 bg-gray-800 rounded-full flex items-center justify-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="text-white w-6 h-6" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <rect x="7" y="3" width="10" height="18" rx="2"></rect>
-                    <line x1="12" y1="17" x2="12" y2="17.01"></line>
-                  </svg>
-                </button>
-                <button className="w-10 h-10 bg-gray-800 rounded-full flex items-center justify-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="text-white w-6 h-6" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <circle cx="12" cy="9" r="6"></circle>
-                    <line x1="12" y1="15" x2="12" y2="21"></line>
-                    <line x1="9" y1="18" x2="15" y2="18"></line>
-                  </svg>
-                </button>
-                <button className="w-10 h-10 bg-gray-800 rounded-full flex items-center justify-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="text-white w-6 h-6" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                    <rect x="3" y="5" width="18" height="14" rx="2"></rect>
-                    <line x1="3" y1="10" x2="21" y2="10"></line>
-                    <line x1="7" y1="15" x2="7.01" y2="15"></line>
-                    <line x1="11" y1="15" x2="13" y2="15"></line>
-                  </svg>
-                </button>
-              </div>
+      {/* iPhone Mockup */}
+      <section
+        className="relative flex items-center justify-center mt-8 md:mt-0 w-[212px] h-[438px] bg-cover bg-center rounded-[35px] border border-white"
+        style={{
+          backgroundImage:
+            "url('https://webdevartur.com/wp-content/uploads/2022/08/ryan-klaus-8QjsdoXDsZs-unsplash-scaled.jpg')",
+        }}
+      >
+        <div className="absolute left-[-2px] top-[81.5px] w-[2px] h-[13px] bg-white rounded opacity-80"></div>
+        <div className="absolute left-[-1.5px] top-[110px] w-[1px] h-[28px] bg-white rounded opacity-80"></div>
+        <div className="absolute left-[-1.5px] top-[148px] w-[1px] h-[28px] bg-white rounded opacity-80"></div>
+        <div className="absolute right-[-2px] top-[121px] w-[2px] h-[45px] bg-white rounded opacity-80"></div>
+        <div className="relative w-full h-full border-6 border-black rounded-[35px] flex items-center justify-center">
+          <div className="absolute top-0 flex items-center justify-center w-[76px] h-[20px] bg-black rounded-b-[15px]">
+            <div className="absolute left-[12px] top-[5px] w-[5px] h-[5px] bg-white bg-opacity-20 rounded-full flex items-center justify-center">
+              <div className="w-[4px] h-[4px] bg-white bg-opacity-30 rounded-full blur-[1px]"></div>
+              <div className="w-[1px] h-[1px] bg-black bg-opacity-70 rounded-full"></div>
+            </div>
+            <div className="absolute top-[-3px] w-[28px] h-[1px] bg-white bg-opacity-20 rounded-full"></div>
+          </div>
+          <div className="absolute top-[34px] w-[12px] h-[10px] bg-white rounded-[2px]">
+            <div className="absolute top-[-7px] left-[1.7px] w-[9px] h-[15px] border border-[1.5px] border-white rounded-[20px]"></div>
+          </div>
+          <div className="absolute top-[46px] text-[42px] font-thin">19:53</div>
+          <div className="absolute top-[92px] text-[10px] font-thin">
+            Tuesday, 9 August
+          </div>
+          <div className="absolute top-[8px] right-[17px] flex items-center space-x-[5px] text-[10px]">
+            <div className="relative flex items-center justify-center w-[14px] h-[7px] border border-white border-opacity-70 rounded-[2px]">
+              <div className="absolute left-[1px] top-[1px] w-[11px] h-[5px] bg-white rounded-[1px]"></div>
+              <div className="absolute right-[-2px] top-[1.5px] w-[1px] h-[3px] bg-white rounded-[2px]"></div>
             </div>
           </div>
+          <div className="absolute left-[29px] bottom-[30px] w-[23px] h-[23px] bg-white bg-opacity-20 rounded-full flex items-center justify-center cursor-pointer transition-transform duration-250 hover:scale-110">
+            <div className="w-[6px] h-[1px] bg-white rounded"></div>
+            <div className="mt-[1px] w-[6px] h-[4px] bg-white rounded-t-[0.5px] rounded-b-[50%]"></div>
+            <div className="mt-[-1px] w-[3px] h-[9px] bg-white rounded"></div>
+            <div className="absolute top-[10px] left-[10.5px] w-[2px] h-[4px] bg-black rounded-full flex items-center justify-center">
+              <div className="w-[1.2px] h-[1.2px] bg-white rounded-full"></div>
+            </div>
+          </div>
+          <div className="absolute right-[29px] bottom-[30px] w-[23px] h-[23px] bg-white bg-opacity-20 rounded-full flex items-center justify-center cursor-pointer transition-transform duration-250 hover:scale-110">
+            <div className="w-[12px] h-[8px] bg-white rounded"></div>
+            <div className="absolute top-[6px] w-[5px] h-[2px] bg-white rounded-t-[1px]"></div>
+            <div className="absolute top-[9px] w-[5px] h-[5px] border border-black rounded-full"></div>
+            <div className="absolute top-[7px] right-[5.2px] w-[5px] h-[5px] bg-black rounded-full transform scale-[0.2]"></div>
+          </div>
+          <div className="absolute bottom-[6px] w-[80px] h-[2px] bg-white bg-opacity-80 rounded-[2px]"></div>
         </div>
+      </section>
+      <div className="absolute left-[35px] top-[35px] text-xs">
+        Codepen by{' '}
+        <a
+          href="https://webdevartur.com/about"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-[#fc7904]"
+        >
+          Artur Burkalo
+        </a>
       </div>
     </div>
   );
