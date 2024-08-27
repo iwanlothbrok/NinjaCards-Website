e589261c7e548883e5529f5dcfbf16b3d86f3098
Iwan <iwaniwanow0345@gmail.com>
2024-08-27 11:44:58 +0300

    contact api was added

---
+++ ninja-cards/pages/api/contact.ts
@@ -0,0 +1,44 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+interface ContactFormData {
+    name: string;
+    email: string;
+    subject: string;
+    phone: string;
+}
+
+const validateEmail = (email: string): boolean => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+};
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        const { name, email, subject, phone }: ContactFormData = req.body;
+
+        if (!name || !email || !subject || !phone) {
+            return res.status(400).json({ error: 'All fields are required' });
+        }
+
+        if (!validateEmail(email)) {
+            return res.status(400).json({ error: 'Invalid email address' });
+        }
+
+        // You can add more validation logic here
+        const contact = await prisma.contact.create({
+            data: {
+                name,
+                email,
+                phone,
+                subject
+            },
+        });
+        // Simulate email sending (you would normally send an email here)
+        return res.status(200).json(contact);
+    } else {
+        res.setHeader('Allow', ['POST']);
+        return res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
