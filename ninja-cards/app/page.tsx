1635974ef08fa89543fcff027d25d1b908458a32
Iwan <iwaniwanow0345@gmail.com>
2024-08-27 15:39:58 +0300

    structure changed

--- ninja-cards/app/page.tsx
+++ ninja-cards/app/page.tsx
@@ -1,30 +1,29 @@
 'use client';
 
 import { HomePage } from './components/layout/Home';
-import Features from './components/Features';
 import About from './components/layout/About';
 
 import Roadmap3 from './components/layout/Roadmap3';
 import VideoTextSection from './components/layout/VideoTextSection';
-import FeaturesSection from './components/FeaturesSection';
 import Testimonies from './components/layout/Testimonies';
 import { motion } from 'framer-motion';
 import ProductGallery from './components/product/ProductCard';
 import FrequentlyAskedQuestions from './components/FrequentlyAskedQuestions';
+import SimpleContactForm from './components/layout/SimpleContactForm';
+import Features from './components/layout/Features';
 
 export default function Home() {
   return (
     <main className="">
       <HomePage />
       <About />
-      <VideoTextSection />
-      {/* <Roadmap /> */}
-      {/* <Roadmap2 /> */}
-      {/* <Roadmap3 /> */}
-      {/* <Testimonies /> */}
-      {/* <ProductGallery /> */}
+      <ProductGallery />
+      <Testimonies />
+      <Roadmap3 />
+      <FrequentlyAskedQuestions searchTerm={''} />
+      {/* <VideoTextSection /> */}
       {/* <FeaturesSection /> */}
-      {/* <FrequentlyAskedQuestions searchTerm={''} /> */}
+      <SimpleContactForm />
     </main>
   );
 }
