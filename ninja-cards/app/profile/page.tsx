be688af4d39528b2b6ed4c99345a885703007115
Iwan <iwaniwanow0345@gmail.com>
2024-09-09 11:37:03 +0300

    [id] page tsx for profiledetails added

--- ninja-cards/app/profile/page.tsx
+++ ninja-cards/app/profile/page.tsx
@@ -12,7 +12,13 @@ import ChangeAddress from '../components/profile/ChangeAdress';
 import ChangeProfileImage from '../components/profile/ChangeProfileImage';
 import QRCodeDownload from '../components/profile/QRCodeDownload';
 
-const TabCard: React.FC<{ tab: string; label: string; description: string; backgroundImage: string; activeTab: string; onClick: () => void }> = ({ tab, label, description, backgroundImage, activeTab, onClick }) => (
+const TabCard: React.FC<{
+    tab: string; 
+    label: string; 
+    backgroundImage: string; 
+    activeTab: string; 
+    onClick: () => void;
+}> = ({ tab, label, backgroundImage, activeTab, onClick }) => (
     <div
         className={`relative flex items-center justify-center p-2 rounded-lg shadow-md border border-gray-700 transition-all duration-300 cursor-pointer text-center bg-cover bg-center
                     ${activeTab === tab ? 'bg-blue-300 text-white' : 'bg-gray-800 text-gray-300'}
@@ -21,9 +27,7 @@ const TabCard: React.FC<{ tab: string; label: string; description: string; backg
         aria-pressed={activeTab === tab}
         style={{ backgroundImage: `url(${backgroundImage})`, backgroundPosition: 'center', backgroundSize: 'cover' }}
     >
-        <div
-            className="absolute inset-0 bg-black opacity-70 rounded-lg"
-        ></div>
+        <div className="absolute inset-0 bg-black opacity-70 rounded-lg"></div>
         <div className="relative z-10">
             <h3 className="text-lg font-semibold mb-1">{label}</h3>
         </div>
@@ -34,7 +38,7 @@ function ProfileContent() {
     const router = useRouter();
     const { user } = useAuth();
     const searchParams = useSearchParams();
-
+    
     // Redirect to login page if no user is logged in
     useEffect(() => {
         if (!user) {
@@ -53,6 +57,7 @@ function ProfileContent() {
         }
     }, [searchParams]);
 
+    // UseMemo to memoize the rendered tab content for performance
     const renderContent = useMemo(() => {
         switch (activeTab) {
             case 'settings':
@@ -78,27 +83,30 @@ function ProfileContent() {
 
     const handleTabClick = useCallback((tab: string) => {
         setLoading(true); // Start loading
+
         if (tab === 'profileDetails') {
-            router.push(`/profileDetails?id=${userId}`);
+            router.push(`/profileDetails/${userId}`);
         } else {
             setActiveTab(tab);
             router.push(`/profile?tab=${tab}`);
         }
     }, [router, userId]);
 
+    // Stop loading once activeTab is updated
     useEffect(() => {
-        setLoading(false); // Stop loading after content is ready
+        setLoading(false);
     }, [activeTab]);
 
     return (
         <div className="flex flex-col items-center pt-32 justify-center min-h-screen p-4 bg-gradient-to-b from-gray-900 via-gray-950 to-black text-gray-200">
             <div className="w-full max-w-3xl">
                 <h2 className="text-2xl font-bold text-white mb-6 text-center">Управление на профила</h2>
+                
+                {/* Tab Navigation */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-8">
                     <TabCard
                         tab="settings"
                         label="Смяна на паролата"
-                        description="Настройки и предпочитания на акаунта"
                         backgroundImage="/settingsBg/profile.png"
                         activeTab={activeTab}
                         onClick={() => handleTabClick('settings')}
@@ -106,7 +114,6 @@ function ProfileContent() {
                     <TabCard
                         tab="changeEmail"
                         label="Смяна на имейла"
-                        description="Промяна на имейл адреса"
                         backgroundImage="/settingsBg/files.png"
                         activeTab={activeTab}
                         onClick={() => handleTabClick('changeEmail')}
@@ -114,7 +121,6 @@ function ProfileContent() {
                     <TabCard
                         tab="changeAddress"
                         label="Смяна на адреса"
-                        description="Промяна на адреса"
                         backgroundImage="/settingsBg/files.png"
                         activeTab={activeTab}
                         onClick={() => handleTabClick('changeAddress')}
@@ -122,7 +128,6 @@ function ProfileContent() {
                     <TabCard
                         tab="changeImage"
                         label="Смяна на профилната снимка"
-                        description="Промяна на профилната снимка"
                         backgroundImage="/settingsBg/settingcash.png"
                         activeTab={activeTab}
                         onClick={() => handleTabClick('changeImage')}
@@ -130,7 +135,6 @@ function ProfileContent() {
                     <TabCard
                         tab="information"
                         label="Лична информация"
-                        description="Преглед и актуализиране на личните данни"
                         backgroundImage="/settingsBg/information.png"
                         activeTab={activeTab}
                         onClick={() => handleTabClick('information')}
@@ -138,7 +142,6 @@ function ProfileContent() {
                     <TabCard
                         tab="links"
                         label="Важни връзки"
-                        description="Управление на външни връзки и връзки"
                         backgroundImage="/settingsBg/laptop.png"
                         activeTab={activeTab}
                         onClick={() => handleTabClick('links')}
@@ -146,7 +149,6 @@ function ProfileContent() {
                     <TabCard
                         tab="preview"
                         label="Преглед на профила"
-                        description="Преглед на профила както го виждат другите"
                         backgroundImage="/settingsBg/settingcash.png"
                         activeTab={activeTab}
                         onClick={() => handleTabClick('preview')}
@@ -154,7 +156,6 @@ function ProfileContent() {
                     <TabCard
                         tab="profileDetails"
                         label="Редактиране на детайлите на профила"
-                        description="Промяна на детайлите на профила"
                         backgroundImage="/settingsBg/files.png"
                         activeTab={activeTab}
                         onClick={() => handleTabClick('profileDetails')}
@@ -162,12 +163,13 @@ function ProfileContent() {
                     <TabCard
                         tab="profileQr"
                         label="Прегледайте вашия QR код"
-                        description="Прегледайте вашия QR код"
                         backgroundImage="/settingsBg/qr.png"
                         activeTab={activeTab}
                         onClick={() => handleTabClick('profileQr')}
                     />
                 </div>
+                
+                {/* Content Section */}
                 <div className="transition-all">
                     {loading ? (
                         <div className="flex justify-center items-center">
