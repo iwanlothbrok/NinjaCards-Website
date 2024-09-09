999c8dcf3e24eefba129f2953cb6ac3edfcff339
Iwan <iwaniwanow0345@gmail.com>
2024-09-09 14:00:53 +0300

    exp with router

--- ninja-cards/app/profileDetails/index.tsx
+++ ninja-cards/app/profileDetails/index.tsx
@@ -1,18 +1,21 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+// File: pages/profileDetails/index.tsx
+
+import { GetServerSideProps } from 'next';
+
+export const getServerSideProps: GetServerSideProps = async ({ query, res }) => {
+  const { id } = query;
+
+  if (id) {
+    res.writeHead(302, {
+      Location: `/profileDetails/${id}`,  // Redirect to /profileDetails/1
+    });
+    res.end();
+  }
+
+  return { props: {} }; // This is required to prevent errors
+};
 
 const ProfileDetailsRedirect = () => {
-  const router = useRouter();
-  const { id } = router.query;
-
-  useEffect(() => {
-    if (id) {
-      // If the `id` exists in the query, redirect to the dynamic route
-      router.push(`/profileDetails/${id}`);
-    }
-  }, [id, router]);
-
-  // You can return a loading state while redirecting
   return (
     <div className="flex justify-center items-center min-h-screen">
       <p>Redirecting...</p>
