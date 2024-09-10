b77a7939d570dfdd2cfc3947fcab73e56710fae9
Iwan <iwaniwanow0345@gmail.com>
2024-09-10 17:21:54 +0300

    Update SocialMediaLinks.tsx

--- ninja-cards/app/components/profileDetails/SocialMediaLinks.tsx
+++ ninja-cards/app/components/profileDetails/SocialMediaLinks.tsx
@@ -4,31 +4,30 @@ import { User } from '../../../types/user'; // Adjust the path based on your fol
 
 interface SocialMediaLinksProps {
     user: User | null;
-    cardStyle: any;
 }
 
-const SocialMediaLinks: React.FC<SocialMediaLinksProps> = ({ user, cardStyle }) => {
+const SocialMediaLinks: React.FC<SocialMediaLinksProps> = ({ user }) => {
     const socialMediaLinks = [
         { key: 'facebook', url: user?.facebook, logo: '/logos/fb.png', label: 'Facebook', gradient: 'from-blue-600 to-purple-600' },
         { key: 'instagram', url: user?.instagram, logo: '/logos/ig.png', label: 'Instagram', gradient: 'from-yellow-500 to-red-700' },
-        // { key: 'linkedin', url: user?.linkedin, logo: '/logos/lk.png', label: 'LinkedIn', gradient: 'from-blue-700 to-blue-900' },
-        // { key: 'twitter', url: user?.twitter, logo: '/logos/x.png', label: 'Twitter', gradient: 'from-blue-500 to-cyan-500' },
-        // { key: 'github', url: user?.github, logo: '/logos/git.png', label: 'GitHub', gradient: 'from-gray-900 to-gray-600' },
-        // { key: 'youtube', url: user?.youtube, logo: '/logos/youtube.png', label: 'YouTube', gradient: 'from-red-600 to-red-400' },
+        { key: 'linkedin', url: user?.linkedin, logo: '/logos/lk.png', label: 'LinkedIn', gradient: 'from-blue-700 to-blue-900' },
+        { key: 'twitter', url: user?.twitter, logo: '/logos/x.png', label: 'Twitter', gradient: 'from-blue-500 to-cyan-500' },
+        { key: 'github', url: user?.github, logo: '/logos/git.png', label: 'GitHub', gradient: 'from-gray-900 to-gray-600' },
+        { key: 'youtube', url: user?.youtube, logo: '/logos/youtube.png', label: 'YouTube', gradient: 'from-red-600 to-red-400' },
         { key: 'tiktok', url: user?.tiktok, logo: '/logos/tiktok.png', label: 'TikTok', gradient: 'from-indigo-600 to-fuchsia-600' },
-        // { key: 'behance', url: user?.behance, logo: '/logos/be.png', label: 'Behance', gradient: 'from-blue-600 to-blue-400' },
-        // { key: 'paypal', url: user?.paypal, logo: '/logos/icons8-paypal48.png', label: 'PayPal', gradient: 'from-blue-500 to-blue-300' },
-        // { key: 'trustpilot', url: user?.trustpilot, logo: '/logos/tp.png', label: 'TrustPilot', gradient: 'from-black to-gray-800' },
-        // { key: 'viber', url: `viber://chat?number=${user?.viber}`, logo: '/logos/viber.png', label: 'Viber', gradient: 'from-purple-500 to-purple-700' },
-        // { key: 'whatsapp', url: `https://wa.me/${user?.whatsapp}`, logo: '/logos/wa.png', label: 'WhatsApp', gradient: 'from-green-600 to-green-700' },
-        // { key: 'website', url: user?.website, logo: '/logos/website.png', label: 'Website', gradient: 'from-cyan-600 to-cyan-800' },
-        // { key: 'revolut', url: `https://revolut.me/${user?.revolut}`, logo: '/logos/rev.png', label: 'Revolut', gradient: 'from-blue-800 to-blue-900' },
+        { key: 'behance', url: user?.behance, logo: '/logos/be.png', label: 'Behance', gradient: 'from-blue-600 to-blue-400' },
+        { key: 'paypal', url: user?.paypal, logo: '/logos/icons8-paypal48.png', label: 'PayPal', gradient: 'from-blue-500 to-blue-300' },
+        { key: 'trustpilot', url: user?.trustpilot, logo: '/logos/tp.png', label: 'TrustPilot', gradient: 'from-black to-gray-800' },
+        { key: 'viber', url: user?.viber ? `viber://chat?number=${user.viber}` : null, logo: '/logos/viber.png', label: 'Viber', gradient: 'from-purple-500 to-purple-700' },
+        { key: 'whatsapp', url: user?.whatsapp ? `https://wa.me/${user.whatsapp}` : null, logo: '/logos/wa.png', label: 'WhatsApp', gradient: 'from-green-600 to-green-700' },
+        { key: 'website', url: user?.website, logo: '/logos/website.png', label: 'Website', gradient: 'from-cyan-600 to-cyan-800' },
+        { key: 'revolut', url: user?.revolut ? `https://revolut.me/${user.revolut}` : null, logo: '/logos/rev.png', label: 'Revolut', gradient: 'from-blue-800 to-blue-900' },
     ];
 
     return (
         <div className="mt-10 text-center">
             <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-5 gap-6">
-                {socialMediaLinks.map((link) => (
+                {socialMediaLinks.map((link) =>
                     link.url && (
                         <a
                             key={link.key}
@@ -46,12 +45,12 @@ const SocialMediaLinks: React.FC<SocialMediaLinksProps> = ({ user, cardStyle }) 
                                     height={48}
                                     className="object-contain transition-all duration-300"
                                     priority
-                                    unoptimized // This will disable image optimization for static exports
+                                    unoptimized
                                 />
                             </div>
                         </a>
                     )
-                ))}
+                )}
             </div>
         </div>
     );
