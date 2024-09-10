cff9f0d5312e3ac5adee7f1abd3084ee4c8f40ce
Iwan <iwaniwanow0345@gmail.com>
2024-09-10 10:42:54 +0300

    secret

--- ninja-cards/next.config.js
+++ ninja-cards/next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     experimental: {
         appDir: true,
     },
+    env: {
+        NEXTAUTH_SECRET: 'your-secret-key',
+    },
     images: {
         loader: 'default',
         domains: [], // No external domains needed
