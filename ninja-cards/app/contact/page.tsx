e5fae698ef2a4206652941cf01a92d6c7fa55e58
Iwan <iwaniwanow0345@gmail.com>
2024-09-12 14:56:32 +0300

    header added in contact form

--- ninja-cards/app/contact/page.tsx
+++ ninja-cards/app/contact/page.tsx
@@ -2,12 +2,16 @@
 
 import React from 'react';
 import ContactForm from './ContactForm';
+import Header from '../components/layout/Header';
 
 const Contact: React.FC = () => {
     return (
-        <main className="flex flex-col items-center justify-center min-h-screen">
+        <>
+            <Header pageInformation='Контакт' textOne='Обратна връзка' textTwo='24/7 поддръжка' textThree='Въпроси' />
+            <main className="flex flex-col items-center justify-center ">
                 <ContactForm />
-        </main>
+            </main>
+        </>
     );
 };
 
