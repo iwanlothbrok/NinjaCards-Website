4b41b2eea7cd16596ca2d550d4c0abda53e4811e
Iwan <iwaniwanow0345@gmail.com>
2024-09-12 14:58:20 +0300

    different margins for the videos

--- ninja-cards/app/askedQuestions/FAQVideos.tsx
+++ ninja-cards/app/askedQuestions/FAQVideos.tsx
@@ -23,7 +23,7 @@ export default function FAQVideos() {
     return (
         <div>
             {/* Секция с карти */}
-            <div className="flex justify-center items-center min-h-screen">
+            <div className="flex justify-center items-center ">
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 p-5 gap-8">
                     <div className="max-w-md">
                         <div className="bg-white shadow-xl rounded-lg overflow-hidden transform transition duration-300 hover:scale-105 hover:shadow-2xl">
@@ -68,7 +68,7 @@ export default function FAQVideos() {
                     </div>
                 </div>
             </div>
-            <header className="mt-6 mb-10 flex flex-col justify-center items-center text-center ">
+            <header className="mt-0 mb-6 p-6 flex flex-col justify-center items-center text-center ">
                 <p className="text-lg max-w-2xl">
                     Имате въпроси? Имаме отговори. Разгледайте нашите често задавани въпроси по-долу или използвайте лентата за търсене, за да намерите това, което търсите.
                 </p>
@@ -76,7 +76,7 @@ export default function FAQVideos() {
                     <input
                         type="text"
                         placeholder="Търсене на отговор на..."
-                        className="w-full px-4 py-2 text-lg text-gray-800 rounded-md shadow-sm"
+                        className="w-full px-6 py-2 text-lg text-gray-800 rounded-md shadow-sm"
                         value={searchTerm}
                         onChange={handleSearchChange}
                     />
