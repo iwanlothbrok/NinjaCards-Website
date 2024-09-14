00b8b8ca855bc3d631a42dfe55fbeefd0dfe22b2
Iwan <iwaniwanow0345@gmail.com>
2024-09-14 11:26:22 +0300

    forget password translated

--- ninja-cards/app/changePassword/page.tsx
+++ ninja-cards/app/changePassword/page.tsx
@@ -18,7 +18,6 @@ export default function ChangePassword() {
         const formData = new FormData(event.currentTarget);
         const email = formData.get('email') as string;
 
-        console.log('inside of submit'); // This should log when the form is submitted
 
         try {
             const res = await fetch(`${BASE_API_URL}/api/auth/forgot-password`, {
@@ -56,7 +55,7 @@ export default function ChangePassword() {
                 <div className="w-full bg-white rounded-lg shadow dark:border sm:max-w-lg dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-12 space-y-6 md:space-y-8 sm:p-14">
                         <h1 className="text-2xl font-bold leading-tight tracking-tight text-gray-900 md:text-3xl dark:text-white">
-                            Reset Password
+                            Забравена Парола
                         </h1>
                         {alert && (
                             <div className={`my-2 w-full p-4 rounded ${alert.color === 'green' ? 'bg-green-500' : 'bg-red-500'} text-white animate-fadeIn`}>
@@ -66,7 +65,7 @@ export default function ChangePassword() {
                         <form className="space-y-6 md:space-y-8" onSubmit={onForgotPasswordSubmit}>
                             <div>
                                 <label htmlFor="email" className="block mb-3 text-base font-medium text-gray-900 dark:text-white">
-                                    Enter your email
+                                    Въведи Имейл
                                 </label>
                                 <input
                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -82,11 +81,11 @@ export default function ChangePassword() {
                                     className="w-full text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-base px-6 py-3 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                                     type="submit"
                                 >
-                                    Reset Password
+                                    Изпрати
                                 </button>
                             </div>
                             <div className="mt-4 text-base text-center text-teal-600 cursor-pointer hover:underline" onClick={() => router.push('/login')}>
-                                Back to Login
+                                Логин
                             </div>
                         </form>
                     </div>
