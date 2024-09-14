b5d58d1710d501757570390280ba83cb5af58072
Iwan <iwaniwanow0345@gmail.com>
2024-09-14 08:58:43 +0300

    login and register buttons were changed

--- ninja-cards/app/register/page.tsx
+++ ninja-cards/app/register/page.tsx
@@ -142,7 +142,7 @@ const Register: React.FC = () => {
 
               <button
                 type="submit"
-                className="w-full text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-base px-6 py-3 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="w-full text-white bg-orange hover:bg-opacity-60 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-base px-6 py-3 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Създай
               </button>
