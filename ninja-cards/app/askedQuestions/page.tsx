a755818f5908dae7c54dac90b49f81342ce9ed35
Iwan <iwaniwanow0345@gmail.com>
2024-09-25 14:13:12 +0300

    search question location changed

--- ninja-cards/app/askedQuestions/page.tsx
+++ ninja-cards/app/askedQuestions/page.tsx
@@ -33,7 +33,20 @@ const FAQPage = () => {
 
       {/* Hero Section */}
       <FAQVideos openModal={openModal} />
-
+      <header className="mt-0 mb-6 p-6 flex flex-col justify-center items-center text-center ">
+        <p className="text-lg max-w-2xl">
+          Имате въпроси? Имаме отговори. Разгледайте нашите често задавани въпроси по-долу или използвайте лентата за търсене, за да намерите това, което търсите.
+        </p>
+        <div className="mt-8 w-full max-w-lg">
+          <input
+            type="text"
+            placeholder="Търсене на отговор на..."
+            className="w-full px-6 py-2 text-lg text-gray-800 rounded-md shadow-sm"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </div>
+      </header>
       {/* FAQ Section */}
       <FrequentlyAskedQuestions searchTerm={searchTerm} />
 
