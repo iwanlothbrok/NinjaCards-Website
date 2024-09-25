e677c8594d77504fd9a3f954a5951dd24c37f9c3
Iwan <iwaniwanow0345@gmail.com>
2024-09-25 14:14:47 +0300

    AllProductsPage

---
+++ ninja-cards/app/products/all/page.tsx
@@ -0,0 +1,61 @@
+"use client"; // Enable client-side rendering
+
+import React, { useEffect, useState } from 'react';
+import ProductList from '../ProductList'; // Adjust this path if necessary
+import { BASE_API_URL } from '@/utils/constants'; // Use your base API URL
+import Header from '@/app/components/layout/Header';
+
+interface Product {
+    id: number;
+    image: string; // Prisma field
+    title: string; // Prisma field
+    price: number;
+    type: string;
+}
+
+const AllProductsPage: React.FC = () => {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await fetch(`${BASE_API_URL}/api/products/allProducts`);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch products');
+                }
+                const data: Product[] = await response.json();
+                console.log(data);
+
+                setProducts(data);
+            } catch (err: any) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+    if (loading) {
+        return <div className="flex justify-center items-center py-72"><img src="/load.gif" alt="Loading..." className="w-40 h-40" /></div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    return (
+        <>
+            <Header pageInformation='Всички Продукти' textOne='NFC Визитки' textTwo='Google Ревюта' textThree='TripAdvisor Ревюта' />
+
+            <div className="container mx-auto  py-12">
+                {products.length === 0 ? (<div className="flex justify-center items-center p-14" style={{ fontSize: '36px' }}>Няма добавени продукти от този тип.</div>) : <ProductList products={products} />}
+            </div>
+        </>
+    );
+};
+
+export default AllProductsPage;
