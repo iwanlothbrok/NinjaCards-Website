13825afc02eee12cdf21abf6037e5e945139fb41
Iwan <iwaniwanow0345@gmail.com>
2025-02-03 10:43:42 +0200

    Update layout.tsx

--- ninja-cards/app/layout.tsx
+++ ninja-cards/app/layout.tsx
@@ -6,7 +6,8 @@ import { AuthProvider } from './context/AuthContext';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Head from 'next/head';
-import { Analytics } from "@vercel/analytics/react"
+import Script from 'next/script'; // ✅ Import `next/script`
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ['latin'], display: 'swap' });
 
@@ -22,6 +23,19 @@ export const metadata: Metadata = {
     locale: 'bg_BG',
   },
   keywords: [
+    'NFC визиткa',
+    'Смарт визитка',
+    'Визитка',
+    'Дигитална визитка',
+    'Eлектронна визитка',
+    'Безконтактна визитка',
+    'Виртуална визитка',
+    'Безконтактна визитка',
+    'NFC карта за бизнес',
+    'Професионална визитка',
+    'Smart vizitki',
+    'Digitalna vizitka',
+    'NFC vizitka',
     'NFC визитни картички',
     'умни визитни картички',
     'Google рецензии NFC',
@@ -31,8 +45,6 @@ export const metadata: Metadata = {
     'NFC продукти',
     'Визитки',
     'Развий бизнеса си',
-    'Tripavisor',
-    'Ревю',
     'Ninja Cards'
   ],
   alternates: {
@@ -48,28 +60,39 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <html lang="bg">
       <Head>
         <meta name="google-site-verification" content="F0YLcxlz9-DNDYvBtQctoP9fkALenaCpKx-iiVhzx3c" />
       </Head>
+
+      {/* ✅ Google Analytics & Google Ads (gtag.js) */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-11395955170"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-11395955170');
+        `}
+      </Script>
+
       <body className={`${inter.className} text-gray-200 antialiased`}>
         <AuthProvider>
           <div className="min-h-screen flex flex-col bg-gradient-to-b from-gray-900 via-gray-950 to-black">
-            {/* Conditionally render the Navbar */}
             <header className="sticky top-0 z-50 bg-gray-900 shadow-md transition-transform duration-300 ease-in-out">
               <Navbar />
             </header>
             <main className="flex-grow">{children}</main>
             <footer className="bg-gradient-to-t from-gray-900 via-gray-950 to-black text-center py-8">
               <p className="text-gray-200">
-                © 2024 Ninja Cards.  Всички права са запазени.
+                © 2024 Ninja Cards. Всички права са запазени.
               </p>
             </footer>
           </div>
-          <Analytics />
-
         </AuthProvider>
       </body>
     </html>
