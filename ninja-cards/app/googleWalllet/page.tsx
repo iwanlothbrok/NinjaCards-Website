6f1018b054717ddd3870de1c86a8f44c8195c0b3
Iwan <iwaniwanow0345@gmail.com>
2025-03-14 10:59:59 +0200

    Create page.tsx

---
+++ ninja-cards/app/googleWalllet/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddToGoogleWallet from '../components/profile/GoogleWallet'
+import { useAuth } from '../context/AuthContext'
+
+export default function page() {
+    const { user } = useAuth()
+    return (
+
+        <div className='justify-center items-center flex flex-col mt-52'>
+            <h1>Your Business Card</h1>
+            <AddToGoogleWallet userId={user?.id || ''} />
+        </div>
+    )
+}
