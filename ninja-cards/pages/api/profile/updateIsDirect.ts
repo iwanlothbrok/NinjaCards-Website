e172ba0522d8bb059fe013fba619d9cbfa14cb8d
Iwan <iwaniwanow0345@gmail.com>
2025-04-02 17:17:36 +0300

    Update updateIsDirect.ts

--- ninja-cards/pages/api/profile/updateIsDirect.ts
+++ ninja-cards/pages/api/profile/updateIsDirect.ts
@@ -19,9 +19,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             return res.status(400).json({ error: 'ID на потребителя е задължително.' });
         }
 
-        const updatedUser = await prisma.user.update({
+        await prisma.user.update({
             where: { id: userId },
             data: { isDirect },
+        });
+        const updatedUser = await prisma.user.findUnique({
+            where: { id: userId },
         });
 
         res.status(200).json({ message: 'Настройката е успешно актуализирана.', user: updatedUser });
