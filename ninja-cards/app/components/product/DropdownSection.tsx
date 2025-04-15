f190975776bc40464f9c6a62076cd07ee44401a5
Iwan <iwaniwanow0345@gmail.com>
2025-04-15 10:20:24 +0300

    Update DropdownSection.tsx

--- ninja-cards/app/components/product/DropdownSection.tsx
+++ ninja-cards/app/components/product/DropdownSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 type DropdownSectionProps = {
     title: string;
@@ -8,15 +9,28 @@ type DropdownSectionProps = {
 };
 
 const DropdownSection: React.FC<DropdownSectionProps> = ({ title, isOpen, setIsOpen, children }) => (
-    <div>
+    <div className="border-t border-b border-gray-600">
         <button
             onClick={() => setIsOpen(!isOpen)}
-            className="w-full text-left font-bold text-lg py-4 border-t border-b border-gray-600 flex justify-between items-center text-orange-400"
+            className="w-full text-left font-bold text-lg py-4 flex justify-between items-center text-orange "
+            aria-expanded={isOpen}
         >
             {title}
-            <span className='text-3xl text-orange'>{isOpen ? '−' : '+'}</span>
+            <span
+                className={`text-3xl transform transition-transform duration-300 ${isOpen ? 'rotate-180' : 'rotate-0'
+                    }`}
+            >
+                ▼
+            </span>
         </button>
-        {isOpen && <div className="text-gray-300 mt-1 px-2">{children}</div>}
+        <motion.div
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: isOpen ? 'auto' : 0, opacity: isOpen ? 1 : 0 }}
+            transition={{ duration: 0.3 }}
+            className="overflow-hidden"
+        >
+            <div className="text-gray-300 mt-1 px-4 py-2">{children}</div>
+        </motion.div>
     </div>
 );
 
