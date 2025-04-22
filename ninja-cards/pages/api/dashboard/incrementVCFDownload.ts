b0269c4a1bd4d5618c8e373915af5a359f0d6ef4
Iwan <iwaniwanow0345@gmail.com>
2025-04-22 11:08:43 +0300

    Update incrementVCFDownload.ts

--- ninja-cards/pages/api/dashboard/incrementVCFDownload.ts
+++ ninja-cards/pages/api/dashboard/incrementVCFDownload.ts
@@ -5,10 +5,10 @@ import cors from '@/utils/cors';
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-      const corsHandled = cors(req, res);
-          if (corsHandled) return;
-      
-          
+    const corsHandled = cors(req, res);
+    if (corsHandled) return;
+
+
     if (req.method !== 'PUT') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
@@ -25,6 +25,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             where: { userId },
             update: { vcfDownloads: { increment: 1 } }, // Increment vcfDownloads
             create: { userId, vcfDownloads: 1 }, // Create with vcfDownloads initialized to 1
+        });
+        await prisma.dashboardEvent.create({
+            data: { 
+            userId, 
+            type: 'download', 
+            timestamp: new Date().toLocaleString('bg-BG', { timeZone: 'Europe/Sofia' }) 
+            },
         });
 
         return res.status(200).json({
