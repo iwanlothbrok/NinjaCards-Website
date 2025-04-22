8ff4b4cab1eb4504b2d1773da3057a0ec7c5ed18
Iwan <iwaniwanow0345@gmail.com>
2025-04-22 11:09:09 +0300

    Update increaseProfileVisits.ts

--- ninja-cards/pages/api/dashboard/increaseProfileVisits.ts
+++ ninja-cards/pages/api/dashboard/increaseProfileVisits.ts
@@ -20,29 +20,41 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     try {
         // Check if the dashboard exists
-        const existingDashboard = await prisma.dashboard.findUnique({
+        // const existingDashboard = await prisma.dashboard.findUnique({
+        //     where: { userId },
+        // });
+
+        // if (!existingDashboard) {
+        //     // Create a new dashboard if it doesn't exist
+        //     await prisma.dashboard.create({
+        //         data: {
+        //             userId,
+        //             profileVisits: 1, // Initialize profileVisits with 1
+        //         },
+        //     });
+
+        //     return res.status(200).json({ message: 'Dashboard created and profile visits initialized to 1' });
+        // }
+
+        // // Increment profile visits
+        // const updatedDashboard = await prisma.dashboard.update({
+        //     where: { userId },
+        //     data: { profileVisits: { increment: 1 } },
+        // });
+
+        const dashboard = await prisma.dashboard.upsert({
             where: { userId },
+            update: { profileVisits: { increment: 1 } },
+            create: { userId, profileVisits: 1 },
         });
 
-        if (!existingDashboard) {
-            // Create a new dashboard if it doesn't exist
-            await prisma.dashboard.create({
-                data: {
-                    userId,
-                    profileVisits: 1, // Initialize profileVisits with 1
-                },
-            });
-
-            return res.status(200).json({ message: 'Dashboard created and profile visits initialized to 1' });
-        }
-
-        // Increment profile visits
-        const updatedDashboard = await prisma.dashboard.update({
-            where: { userId },
-            data: { profileVisits: { increment: 1 } },
+        await prisma.dashboardEvent.create({
+            data: {
+                userId, type: 'visit', timestamp: new Date().toLocaleString('bg-BG', { timeZone: 'Europe/Sofia' })
+            },
         });
 
-        return res.status(200).json({ message: 'Profile visits incremented', updatedDashboard });
+        return res.status(200).json({ message: 'Visit tracked', dashboard });
     } catch (error) {
         console.error('Error incrementing profile visits:', error);
         return res.status(500).json({ error: 'Failed to increment profile visits' });
