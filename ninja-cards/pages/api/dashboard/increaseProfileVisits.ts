e45137de8e6e13f946e0a0f61e5358a77b1f47c8
Iwan <iwaniwanow0345@gmail.com>
2025-04-22 11:48:27 +0300

    Update increaseProfileVisits.ts

--- ninja-cards/pages/api/dashboard/increaseProfileVisits.ts
+++ ninja-cards/pages/api/dashboard/increaseProfileVisits.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
 import cors from '@/utils/cors';
+import { getTime } from '@/utils/bgTime';
+
 
 const prisma = new PrismaClient();
 
@@ -13,35 +15,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     const { userId } = req.body;
-
     if (!userId) {
         return res.status(400).json({ error: 'User ID is required' });
     }
 
     try {
-        // Check if the dashboard exists
-        // const existingDashboard = await prisma.dashboard.findUnique({
-        //     where: { userId },
-        // });
-
-        // if (!existingDashboard) {
-        //     // Create a new dashboard if it doesn't exist
-        //     await prisma.dashboard.create({
-        //         data: {
-        //             userId,
-        //             profileVisits: 1, // Initialize profileVisits with 1
-        //         },
-        //     });
-
-        //     return res.status(200).json({ message: 'Dashboard created and profile visits initialized to 1' });
-        // }
-
-        // // Increment profile visits
-        // const updatedDashboard = await prisma.dashboard.update({
-        //     where: { userId },
-        //     data: { profileVisits: { increment: 1 } },
-        // });
-
         const dashboard = await prisma.dashboard.upsert({
             where: { userId },
             update: { profileVisits: { increment: 1 } },
@@ -50,7 +28,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
         await prisma.dashboardEvent.create({
             data: {
-                userId, type: 'visit', timestamp: new Date().toLocaleString('bg-BG', { timeZone: 'Europe/Sofia' })
+                userId,
+                type: 'visit',
+                timestamp: getTime(),
             },
         });
 
