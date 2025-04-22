d6ba5633036a85d3ca55fc2dcf81e64e6059e60c
Iwan <iwaniwanow0345@gmail.com>
2025-04-22 11:08:56 +0300

    Update incrementProfileShares.ts

--- ninja-cards/pages/api/dashboard/incrementProfileShares.ts
+++ ninja-cards/pages/api/dashboard/incrementProfileShares.ts
@@ -5,10 +5,10 @@ import cors from '@/utils/cors';
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-      const corsHandled = cors(req, res);
-          if (corsHandled) return;
-      
-          
+    const corsHandled = cors(req, res);
+    if (corsHandled) return;
+
+
     if (req.method !== 'PUT') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
@@ -21,16 +21,32 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     try {
         // Use upsert to ensure the dashboard exists
+        // const dashboard = await prisma.dashboard.upsert({
+        //     where: { userId },
+        //     update: { profileShares: { increment: 1 } }, // Increment vcfDownloads
+        //     create: { userId, profileShares: 1 }, // Create with vcfDownloads initialized to 1
+        // });
+
+        // return res.status(200).json({
+        //     message: 'Profile Shares incremented successfully',
+        //     dashboard,
+        // });
+
         const dashboard = await prisma.dashboard.upsert({
             where: { userId },
-            update: { profileShares: { increment: 1 } }, // Increment vcfDownloads
-            create: { userId, profileShares: 1 }, // Create with vcfDownloads initialized to 1
+            update: { profileShares: { increment: 1 } },
+            create: { userId, profileShares: 1 },
         });
 
-        return res.status(200).json({
-            message: 'Profile Shares incremented successfully',
-            dashboard,
+        await prisma.dashboardEvent.create({
+            data: {
+                userId, type: 'share', timestamp: new Date().toLocaleString('bg-BG', { timeZone: 'Europe/Sofia' })
+            },
         });
+
+        return res.status(200).json({ message: 'Profile shared tracked', dashboard });
+
+
     } catch (error) {
         console.error('Error incrementing VCF download count:', error);
         return res.status(500).json({ error: 'Failed to increment VCF download count' });
