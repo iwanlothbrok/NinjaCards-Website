0f98c80d5aa3d9b35bc0f045095c144bd84a42a5
Iwan <iwaniwanow0345@gmail.com>
2025-05-24 11:10:32 +0300

    Update page.tsx

--- ninja-cards/app/login/page.tsx
+++ ninja-cards/app/login/page.tsx
@@ -81,7 +81,7 @@ const Login: React.FC = () => {
                                 <input
                                     type="email"
                                     id="email"
-                                    className={`border border-gray-300 text-base rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 ${errors.email ? "border-red-500" : ""}`}
+                                    className={`border  text-base rounded-lg  block w-full p-3 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 ${errors.email ? "border-red-500" : ""}`}
                                     placeholder="вашият@имейл.com"
                                     {...register("email")}
                                     required
