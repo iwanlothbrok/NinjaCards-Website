72d43eb9a2ecddf7b18c6b486914b51d2da3ac68
Iwan <iwaniwanow0345@gmail.com>
2026-02-06 10:00:30 +0200

    Update subscription.ts

--- ninja-cards/types/subscription.ts
+++ ninja-cards/types/subscription.ts
@@ -2,9 +2,39 @@ export interface Subscription {
     id: string;
     subscription_id: string;   // Stripe sub id
     stripe_user_id: string;    // Stripe customer id
-    plan_id: string;
-    status: "active" | "trialing" | "cancelled" | string;
+
+    status: SubscriptionStatus;
+    plan: PlanType;
+
     start_date: string;
     end_date: string | null;
+    cancel_date: string | null;
+
     userId: string;
+    invoices: Invoice[];
 };
+interface Invoice {
+    id: string;
+    stripeId: string;
+    userId: string;
+    amountPaid: number;
+    currency: string;
+    hostedInvoiceUrl: string;
+    createdAt: Date;
+
+}
+
+enum PlanType {
+    SHINOBI = "SHINOBI",
+    SAMURAI = "SAMURAI",
+    SHOGUN = "SHOGUN",
+}
+
+enum SubscriptionStatus {
+    active,
+    trialing,
+    past_due,
+    unpaid,
+    paused,
+    cancelled,
+}
